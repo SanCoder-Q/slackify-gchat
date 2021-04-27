591fde2142cabcb91e450a114a1b07d083fcdf3e
Jianming Qu <jmqu@thoughtworks.com>
2021-04-27 18:19:25 +0800

    init

---
+++ ci/build.js
@@ -0,0 +1,39 @@
+var fs = require('fs'),
+    archiver = require('archiver'),
+    manifestPath = `${__dirname}/../manifest.json`,
+    buildPath = `${__dirname}/../dist/build`,
+    compiledPath = `${__dirname}/../dist/compiled`,
+    manifest = JSON.parse(fs.readFileSync(manifestPath));
+
+var isLocalBuild = process.env.TRAVIS_BRANCH === undefined;
+var BUILD_VERSION = process.argv[2];
+
+addManifest();
+pack();
+
+function addManifest() {
+  if (!isLocalBuild) {
+    var version = manifest.version.split('.');
+    manifest.version = `${version[0]}.${version[1]}.${BUILD_VERSION}`;
+    fs.writeFileSync(`${compiledPath}/manifest.json`, JSON.stringify(manifest, null, 4));
+  } else {
+    fs.writeFileSync(`${compiledPath}/manifest.json`, JSON.stringify(manifest, null, 4));
+  }
+  console.log('Current Version: ' + manifest.version);
+}
+
+function pack() {
+  console.log('Zipping ...');
+  fs.existsSync(buildPath) || fs.mkdirSync(buildPath);
+  var output = fs.createWriteStream(`${buildPath}/tab-switcher.zip`);
+  var archive = archiver('zip');
+  output.on('close', function() {
+    console.log(`Artifact: build/slackify-gchat.zip(${archive.pointer()} bytes)`);
+  });
+  archive.on('error', function(err) {
+    throw err;
+  });
+  archive.pipe(output);
+  archive.glob(`${compiledPath}/**/*`);
+  archive.finalize();
+}
---
+++ src/ts/main.ts
@@ -0,0 +1,5 @@
+import TabActiveInfo = chrome.tabs.TabActiveInfo;
+import ChromeWindow = chrome.windows.Window;
+import ChromeTab = chrome.tabs.Tab;
+
+console.log('hello, world');
